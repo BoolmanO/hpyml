c7399313e328a189a252e2ad88bcbf147423f05d
Boolman0 <ponos585858@gmail.com>
2022-04-12 12:55:16 +0600

    First commit!

---
+++ Examples/examp1.py
@@ -0,0 +1,72 @@
+
+from hpyml import hpyml
+
+#старт, создание переменной для hpyml, обязателен
+#start, creating a variable for hpyml, required
+pyml = hpyml()
+
+#метод start принимает аргументы title,filename(название html файла),css_name(название css файла)
+#название вводится без расширения
+
+#start method takes arguments title,filename(html file name),css_name(css file name)
+#name is entered without extension
+
+pyml.start()
+
+
+#создание переменной для блока1 #creating a variable for block1
+#creating a variable for block1
+block1 = pyml.block(pyml)
+
+
+
+#метод open принимает в себе такие параметры как block(тег) div или p (<> не нужен), class_ класс объекта, attrs атрибуты, text текст
+#по умолчанию div,None,"","" (то есть пустота и тег див)
+
+#open method accepts such parameters as block(tag) div or p (<> not needed), class_ object class, attrs attributes, text text
+#default div,None,"","" (i.e. empty and div tag)
+
+block1.open(class_="main",text="Example text!")
+
+
+
+#простое массовое создание тега и текста в нем
+#simple mass creation of a tag and text in it
+#                 tag      text         тег      текст
+pyml.html_text({ "p" : "2nd example", "h2" : "3th example" })
+
+
+#закрытие тега, не принимает параметров
+#closing tag, takes no parameters
+block1.exit()
+
+#время для CSS!
+#time for CSS!
+
+
+#метод css принимает аргументы в виде словаря {.class или просто tag : {cвойство : параметр свойства,свойство : параметр свойства}}
+#css method accepts dictionary arguments {.class simply or tag : {property : parameter,property : property parameter}}
+
+params_main = {
+"text-align" : "center",
+"padding" : "15px",
+"margin" : "15px",
+"background" : "rgb(0,0,0,0.6)",
+"border-radius" : "15px",
+"color" : "rgb(255,255,255,0.8)"
+}
+
+params_body = {
+    "background" : "rgb(0,0,0,0.4)"
+}
+pyml.css( {".main": params_main, "body" : params_body} )
+
+#это равносильно pyml.css({"body" : params_body}) и pyml.css({".main" : params_main})
+#this is equivalent to pyml.css({"body" : params_body}) and pyml.css({".main" : params_main})
+
+
+#обязательное закрытие html документа!
+#obligatory closing of the html document!
+pyml.end()
+
+
---
+++ Examples/examp2.py
@@ -0,0 +1,84 @@
+from hpyml import hpyml
+#read examp1
+#прочитайте examp1
+
+
+pyml = hpyml()
+
+#give names for files | filename - html, css_name - css
+#раздача имен файлам | filename - html, css_name - css
+
+pyml.start(filename="textfile", css_name = "designfile")
+
+main = pyml.block(pyml)
+type_button = pyml.block(pyml)
+button = pyml.block(pyml)
+footer = pyml.block(pyml)
+small = pyml.block(pyml)
+a = pyml.block(pyml)
+space = pyml.block(pyml)
+
+main.open("div",class_="main")
+pyml.html_text({"h2" : "Why hpyml is good?","h6": "IDK, lol"})
+type_button.open(block="p",class_="button")
+
+#                             атрибуты стоит помещать в '', их значения в ""
+#                             attributes should be put in '', their values ​​in ""
+#                              ↓attr       ↓val↓      attr↓
+button.open(block="a",attrs = 'href = "http://www.yandex.ru"',text="link to yandex!")
+button.exit()
+type_button.exit()
+main.exit()
+
+space.open(block="br")
+
+
+footer.open(block="div",class_="footer")
+small.open(block="small")
+a.open(block="a",text="Contact us!",attrs = 'href = "https://www.youtube.com/"')
+
+a.exit()
+small.exit()
+footer.exit()
+
+
+
+pyml.end()
+
+#CSS!!!
+pyml.css({
+    "body" : {"background" : "rgb(0,0,0,0.6)"}
+})
+
+pyml.css({
+    ".main" : {
+    "text-align" : "center",
+    "background" : "rgb(0,0,0,0.7)",
+    "border-radius":"15px",
+    "color" : "rgb(255,255,255,0.8)",
+    "padding" : "15px",
+    "margin" : "15px",
+    "box-shadow" : "0px 0px 5px 5px rgb(0,0,0,0.8)",
+    "text-decoration" : "none"}
+})
+
+pyml.css({
+    ".main:hover" : {
+    "color" : "rgb(155,155,0)",
+    "text-align" : "center",
+    "text-shadow" : " 0px 0px 15px rgb(0,0,0)"}
+})
+
+pyml.css({
+    ".footer" : {
+    "text-align" : "center",
+    "background" : "rgb(0,0,0,0.7)",
+    "border-radius":"15px",
+    "padding" : "10px",
+    "margin" : "10px",
+    "box-shadow" : "0px 0px 5px 5px rgb(0,0,0,0.8)"
+    }
+})
+
+
+
